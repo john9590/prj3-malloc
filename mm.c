6a2b881c802a3d63f6b7fdb663a557a512a6f007
john9590 <john9590@naver.com>
2023-06-10 17:53:38 -0700

    reset

--- mm.c
+++ mm.c
@@ -40,6 +40,60 @@ team_t team = {
 
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
 
+#define NEXT_FITx
+
+/* $begin mallocmacros */
+/* Basic constants and macros */
+#define WSIZE       4       /* Word and header/footer size (bytes) */ //line:vm:mm:beginconst
+#define DSIZE       8       /* Double word size (bytes) */
+#define CHUNKSIZE  (1<<4)  /* Extend heap by this amount (bytes) */  //line:vm:mm:endconst 
+
+#define MAX(x, y) ((x) > (y)? (x) : (y))  
+
+/* Pack a size and allocated bit into a word */
+#define PACK(size, alloc)  ((size) | (alloc)) //line:vm:mm:pack
+
+/* Read and write a word at address p */
+#define GET(p)       (*(unsigned int *)(p))            //line:vm:mm:get
+#define PUT(p, val)  (*(unsigned int *)(p) = (val))    //line:vm:mm:put
+
+/* Read the size and allocated fields from address p */
+#define GET_SIZE(p)  (GET(p) & ~0x7)                   //line:vm:mm:getsize
+#define GET_ALLOC(p) (GET(p) & 0x1)                    //line:vm:mm:getalloc
+
+/* Given block ptr bp, compute address of its header and footer */
+#define HDRP(bp)       ((char *)(bp) - WSIZE)                      //line:vm:mm:hdrp
+#define FTRP(bp)       ((char *)(bp) + GET_SIZE(HDRP(bp)) - DSIZE) //line:vm:mm:ftrp
+#define PVRP(bp)       (char *)(bp)
+#define NTRP(bp)       ((char *)(bp) + WSIZE)
+
+/* Given block ptr bp, compute address of next and previous blocks */
+#define NEXT_BLKP(bp)  ((char *)(bp) + GET_SIZE(((char *)(bp) - WSIZE))) //line:vm:mm:nextblkp
+#define PREV_BLKP(bp)  ((char *)(bp) - GET_SIZE(((char *)(bp) - DSIZE))) //line:vm:mm:prevblkp
+
+#define NEXT_BLNK(bp)  (*(void **)((bp)+WSIZE))//GET((char *)(NTRP(bp)))//line:vm:mm:nextblkp
+#define PREV_BLNK(bp)  (*(void **)(bp))//GET((char *)(PVRP(bp)))//line:vm:mm:prevblkp
+
+//#define NEXT_BLNK(bp)  GET((char *)(bp))
+/* $end mallocmacros */
+/*
+ * If NEXT_FIT defined use next fit search, else use first-fit search 
+ */
+
+/* Global variables */
+static char *heap_listp = 0;  /* Pointer to first block */  
+static char *heap_last = 0;
+static char *heap_max_size = 0;
+
+static char *seg_listp = 0;
+static void *extend_heap(size_t words);
+static void place(void *bp, size_t asize);
+static void *find_fit(size_t asize);
+static void *coalesce(void *bp);
+static void printblock(void *bp); 
+static void checkheap(int verbose);
+static void checkblock(void *bp);
+
 void *size_find(int size) {
     static char* result;
 	int i = 0;
@@ -118,7 +172,6 @@ void *mm_malloc(size_t size)
         asize = 2*DSIZE;                                        //line:vm:mm:sizeadjust2
     else
         asize = DSIZE * ((size + (DSIZE) + (DSIZE-1)) / DSIZE); //line:vm:mm:sizeadjust3
-
     /* Search the free list for a fit */
     if ((bp = find_fit(asize)) != NULL) {  //line:vm:mm:findfitcall
         place(bp, asize);                  //line:vm:mm:findfitplace
--- mm.h
+++ mm.h
@@ -1,69 +1,11 @@
 #include <stdio.h>
-#define NEXT_FITx
-
-/* $begin mallocmacros */
-/* Basic constants and macros */
-#define WSIZE       4       /* Word and header/footer size (bytes) */ //line:vm:mm:beginconst
-#define DSIZE       8       /* Double word size (bytes) */
-#define CHUNKSIZE  (1<<4)  /* Extend heap by this amount (bytes) */  //line:vm:mm:endconst 
-
-#define MAX(x, y) ((x) > (y)? (x) : (y))  
-
-/* Pack a size and allocated bit into a word */
-#define PACK(size, alloc)  ((size) | (alloc)) //line:vm:mm:pack
-
-/* Read and write a word at address p */
-#define GET(p)       (*(unsigned int *)(p))            //line:vm:mm:get
-#define PUT(p, val)  (*(unsigned int *)(p) = (val))    //line:vm:mm:put
-
-/* Read the size and allocated fields from address p */
-#define GET_SIZE(p)  (GET(p) & ~0x7)                   //line:vm:mm:getsize
-#define GET_ALLOC(p) (GET(p) & 0x1)                    //line:vm:mm:getalloc
-
-/* Given block ptr bp, compute address of its header and footer */
-#define HDRP(bp)       ((char *)(bp) - WSIZE)                      //line:vm:mm:hdrp
-#define FTRP(bp)       ((char *)(bp) + GET_SIZE(HDRP(bp)) - DSIZE) //line:vm:mm:ftrp
-#define PVRP(bp)       (char *)(bp)
-#define NTRP(bp)       ((char *)(bp) + WSIZE)
-
-/* Given block ptr bp, compute address of next and previous blocks */
-#define NEXT_BLKP(bp)  ((char *)(bp) + GET_SIZE(((char *)(bp) - WSIZE))) //line:vm:mm:nextblkp
-#define PREV_BLKP(bp)  ((char *)(bp) - GET_SIZE(((char *)(bp) - DSIZE))) //line:vm:mm:prevblkp
-
-#define NEXT_BLNK(bp)  (*(void **)((bp)+WSIZE))//GET((char *)(NTRP(bp)))//line:vm:mm:nextblkp
-#define PREV_BLNK(bp)  (*(void **)(bp))//GET((char *)(PVRP(bp)))//line:vm:mm:prevblkp
-
-//#define NEXT_BLNK(bp)  GET((char *)(bp))
-/* $end mallocmacros */
-/*
- * If NEXT_FIT defined use next fit search, else use first-fit search 
- */
-
-/* Global variables */
-static char *heap_listp = 0;  /* Pointer to first block */  
-static char *heap_last = 0;
-static char *heap_max_size = 0;
-
-static char *seg_listp = 0;
-
-#ifdef NEXT_FIT
-static char *rover;           /* Next fit rover */
-#endif
 
 extern int mm_init (void);
-extern void mm_deinit(void);
 extern void *mm_malloc (size_t size);
 extern void mm_free (void *ptr);
 extern void *mm_realloc(void *ptr, size_t size);
 
-/* Function prototypes for internal helper routines */
-static void *extend_heap(size_t words);
-static void place(void *bp, size_t asize);
-static void *find_fit(size_t asize);
-static void *coalesce(void *bp);
-static void printblock(void *bp); 
-static void checkheap(int verbose);
-static void checkblock(void *bp);
+
 /* 
  * Students work in teams of one or two.  Teams enter their team name, 
  * personal names and login IDs in a struct of this
